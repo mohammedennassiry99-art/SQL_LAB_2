c52a2cec5d066176c8bae0978bff15051059e41b
Mohammed <mohammedennassiry99@gmail.com>
2025-12-03 19:43:09 +0100

    hello

---
+++ lab2_schema.sql
@@ -0,0 +1,135 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bibliotheque
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `abonne`
+--
+
+DROP TABLE IF EXISTS `abonne`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `abonne` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `abonne`
+--
+
+LOCK TABLES `abonne` WRITE;
+/*!40000 ALTER TABLE `abonne` DISABLE KEYS */;
+/*!40000 ALTER TABLE `abonne` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `auteur`
+--
+
+DROP TABLE IF EXISTS `auteur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auteur` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auteur`
+--
+
+LOCK TABLES `auteur` WRITE;
+/*!40000 ALTER TABLE `auteur` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auteur` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `emprunt`
+--
+
+DROP TABLE IF EXISTS `emprunt`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `emprunt` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ouvrage_id` int NOT NULL,
+  `abonne_id` int NOT NULL,
+  `date_debut` date NOT NULL,
+  `date_fin` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_emprunt_ouvrage` (`ouvrage_id`),
+  KEY `fk_emprunt_abonne` (`abonne_id`),
+  KEY `date_fin` (`date_fin`),
+  CONSTRAINT `fk_emprunt_abonne` FOREIGN KEY (`abonne_id`) REFERENCES `abonne` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_emprunt_ouvrage` FOREIGN KEY (`ouvrage_id`) REFERENCES `ouvrage` (`id`) ON DELETE RESTRICT,
+  CONSTRAINT `ck_date_emprunt` CHECK (((`date_fin` is null) or (`date_fin` >= `date_debut`)))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `emprunt`
+--
+
+LOCK TABLES `emprunt` WRITE;
+/*!40000 ALTER TABLE `emprunt` DISABLE KEYS */;
+/*!40000 ALTER TABLE `emprunt` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ouvrage`
+--
+
+DROP TABLE IF EXISTS `ouvrage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ouvrage` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titre` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `disponible` tinyint(1) NOT NULL DEFAULT '1',
+  `auteur_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_ouvrage_auteur` (`auteur_id`),
+  KEY `disponible` (`disponible`),
+  CONSTRAINT `fk_ouvrage_auteur` FOREIGN KEY (`auteur_id`) REFERENCES `auteur` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ouvrage`
+--
+
+LOCK TABLES `ouvrage` WRITE;
+/*!40000 ALTER TABLE `ouvrage` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ouvrage` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-03 19:38:30
